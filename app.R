6ae137be45904714cac6e5472971563ed50dd406
Adrian Cockcroft <adrian.cockcroft@gmail.com>
2020-11-13 15:50:56 -1000

    Cleaned up speed and temperature tabs, data correctly sorted.

--- app.R
+++ app.R
@@ -20,8 +20,10 @@ ptf <- function(trackfile) {
     }
     # collapse data by averaging repeated location points, reduces size to about a fifth
     tf <<- collapv(rtf, c(1,5,4))
+    # re-sort by lap and time
+    tf <<- tf[order(tf[,1],tf[,2]),]
     # remove lap 0
-    tf <<- rtf[(rtf[,1]!=0),]
+    tf <<- tf[(tf[,1]!=0),]
     # create list of separate laps
     laps <<- split(tf, tf[,1])
     lapl <- lapply(laps, plap)
@@ -57,17 +59,24 @@ ui <- fluidPage(
         sidebarPanel(
             titlePanel("Shiny Tesla Telemetry Analyzer"),
             tags$a(href="github.com/adrianco/rs-tesla-telemetry", "github.com/adrianco/rs-tesla-telemetry"),
-            h3("Lap Picker"),
+            h3("Pick laps to analyze"),
             DTOutput("laplist")
         ),
         mainPanel(
             tabsetPanel(
-                tabPanel("Laps",
-                        h3("Acceleration (red) and deceleration (blue) point by point, click for speed and lateral G"),
-                        # tabular summary of laps
-                        DTOutput("sumtab"),
+                tabPanel("Speed",
+                        h3("Acceleration (red) and deceleration (blue) point by point, hover for speed and lateral G"),
+                        # speed oriented summary of selected laps
+                        DTOutput("speedtab"),
                         # Show a map of the track with lap highlighted
                         leafletOutput("map", height=600)
+                ),
+                tabPanel("Temperature",
+                         h3("Battery temperature green, orange, red point by point, hover for details"),
+                         # temperature oriented summary of selected laps
+                         DTOutput("temptab"),
+                         # Show a map of the track with lap highlighted
+                         leafletOutput("tempmap", height=600)
                 ),
                 tabPanel("Plots"),
                 tabPanel("Turns")
@@ -95,11 +104,8 @@ server <- function(input, output) {
     })
     
     # Speed tab table and map
-    output$sumtab <- renderDT({
-        #if no rows are selected all are used
-        rows<-input$laplist_rows_selected
-        if (length(rows)==0) rows<-1:length(lapdf$lap)
-        datatable(lapdf[rows,1:11], selection=list(selected=input$laplist_rows_selected, mode='none'),
+    output$speedtab <- renderDT({
+        datatable(lapdf[input$laplist_rows_selected, 2:11], selection=list(selected=1, mode='single'),
                   options=list(pageLength=5, ordering=FALSE, initComplete=htmlwidgets::JS(
                       "function(settings, json) {",
                       "$(this.api().table().container()).css({'font-size': '80%'});",
@@ -109,19 +115,48 @@ server <- function(input, output) {
     })
     
     output$map <- renderLeaflet({
-        lapc <- input$sumtab_rows_selected
-        if (length(lapc) == 0) {
-            lapc <- 1:length(laps)
-        }
-        tfl <- tf[tf$Lap %in% lapc,]
-        leaflet(tfl) %>% addTiles() %>%
+        if (length(input$laplist_rows_selected) > 0 && length(input$speedtab_rows_selected) > 0) {
+        leaflet(laps[[input$laplist_rows_selected[input$speedtab_rows_selected]]]) %>% addTiles() %>%
             fitBounds(min(tf[5]),  min(tf[4]), max(tf[5]), max(tf[4])) %>%
             addCircles(lng=turns$lng, lat=turns$lat, popup=row.names(turns), color="black", radius=turns$radius) %>%
             addCircles(lng=~Longitude..decimal., lat=~Latitude..decimal., radius=1,
                        color=~accelcolor(Longitudinal.Acceleration..m.s.2.),
                        label=~paste(Speed..MPH., "mph ", round(Lateral.Acceleration..m.s.2./gms, 2), "G"))
+        }
     })
 
+    # Temperature tab table and map
+    output$temptab <- renderDT({
+        datatable(lapdf[input$laplist_rows_selected, c(5,10:15)], selection=list(selected=1, mode='single'),
+                  options=list(pageLength=5, ordering=FALSE, initComplete=htmlwidgets::JS(
+                      "function(settings, json) {",
+                      "$(this.api().table().container()).css({'font-size': '80%'});",
+                      "}")
+                  )
+        )
+    })
+    
+    # colour the circles
+    tempcolor <- function(pct) {
+        ifelse(pct > 1.0, "red", ifelse(pct > 0.8, "orange", "green"))
+    }
+    
+    
+    output$tempmap <- renderLeaflet({
+        if (length(input$laplist_rows_selected) > 0 && length(input$temptab_rows_selected) > 0) {
+        leaflet(laps[[input$laplist_rows_selected[input$temptab_rows_selected]]]) %>% addTiles() %>%
+            fitBounds(min(tf[5]),  min(tf[4]), max(tf[5]), max(tf[4])) %>%
+            addCircles(lng=turns$lng, lat=turns$lat, popup=row.names(turns), color="black", radius=turns$radius) %>%
+            addCircles(lng=~Longitude..decimal., lat=~Latitude..decimal., radius=1,
+                       color=~tempcolor(Battery.Temp....),
+                       label=~paste(" Battery:", round(Battery.Temp....,2),
+                                    "Brakes:", round(Brake.Temperature.Front.Left....est..,2), round(Brake.Temperature.Front.Right....est..,2),
+                                    round(Brake.Temperature.Rear.Left....est..,2), round(Brake.Temperature.Rear.Right....est..,2),
+                                    " InvertersFR:", round(Front.Inverter.Temp....,2), round(Rear.Inverter.Temp....,2)
+                                    )
+            )
+        }
+    })
 }
 
 # Run the application
