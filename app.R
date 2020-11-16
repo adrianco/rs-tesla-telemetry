4d42ff7e2f693db1d2b4d84a18dae9c9d0bdb155
Adrian Cockcroft <adrian.cockcroft@gmail.com>
2020-11-16 01:12:08 -0800

    Basic plotting and labeling works

--- app.R
+++ app.R
@@ -27,12 +27,30 @@ ptf <- function(trackfile) {
     tf <<- collapv(rtf, c(1,5,4))
     # re-sort by lap and time
     tf <<- tf[order(tf[,1],tf[,2]),]
-    # remove lap 0
-    tf <<- tf[(tf[,1]!=0),]
-    # create list of separate laps
-    laps <<- split(tf, tf[,1])
-    lapl <- lapply(laps, plap)
-    lapdf <<- data.frame(t(sapply(lapl,c)))
+    # remove incomplete laps
+    latspan <-  max(tf[4]) - min(tf[4])
+    longspan <- max(tf[5]) - min(tf[5])
+    lapmax <<- max(tf$Lap) # max lap count in file (which starts at Lap 0)
+    lapcnt <<- 0    # number of good laps in the file
+    laps <<- list() # reset global empty list
+    for (i in 1:lapmax) { # ignore lap 0
+        l <- tf[tf$Lap==i,] # get the data for one lap
+        ln <- length(l$Lap) # get the number of data points
+        # does the lap go all the way across the circuit and start end nearly the same place? 
+        if (((max(l[,4]) - min(l[,4]))/latspan > 0.95) &&
+            ((max(l[,5]) - min(l[,5]))/longspan > 0.95) &&
+            (abs(l[1,4] - l[ln,4]) < 0.0001) && (abs(l[1,5] - l[ln,5]) < 0.0001)) {
+                lapcnt <<- lapcnt+1
+                laps[[lapcnt]] <<- l # only append good laps to the global list
+        }
+    }
+    lapl <- lapply(laps, plap) # run plap against the list of laps and accumulate results
+    lapdf <<- data.frame(t(sapply(lapl,c))) # convert to data frame - looked this up on stack overflow...
+    # sort the list of laps and summary data to have fastest first
+    o <-order(as.numeric(lapdf$seconds))
+    laps <<- laps[o]
+    lapdf <<- lapdf[o,]
+    row.names(lapdf) <<- lapdf$lapnum  # otherwise it numbers sequentially
 }
 
 # process a lap
@@ -57,6 +75,7 @@ plap <- function(lap) {
     return(lapinfo)
 }
 
+
 # Define UI for application to summarize Tesla track data
 ui <- fluidPage(
     # Application title
@@ -64,7 +83,7 @@ ui <- fluidPage(
         sidebarPanel(
             titlePanel("Shiny Tesla Telemetry Analyzer"),
             tags$a(href="github.com/adrianco/rs-tesla-telemetry", "github.com/adrianco/rs-tesla-telemetry"),
-            h4("Pick laps to analyze from:"),
+            h4("Pick complete laps to compare sorted fastest first from:"),
             p(filename),
             DTOutput("laplist")
         ),
@@ -91,7 +110,8 @@ ui <- fluidPage(
                          plotOutput("speedplot", height='600px'),
                          #plotOutput("tempplot")
                 ),
-                tabPanel("Turns")
+                tabPanel("Turns"),
+                tabPanel("Metadata")
             )
         )
     )
@@ -176,31 +196,50 @@ server <- function(input, output) {
         }
     })
     
- 
+    # map the columns in lapdf that we use in the plot table back to underlying columns for plotting
+    colmap <- matrix(c(3,4, 5,7,8,10,12,14,15,
+                       3,3,13,6,7, 9,19,24,25), nrow=2, ncol=9, byrow=TRUE)
     
+    # take a picked column and return the index into the lap data
+    unpick <- function(col) {
+        colmap[2,col]
+    }
+    
+    # plotting tab
     output$plottab <- renderDT({
-        datatable(lapdf[input$laplist_rows_selected, c(3,4,5,7,8,9,12,15)], selection=list(selected=matrix(c(1,2),1), mode='multiple', target='cell'),
+        datatable(lapdf[input$laplist_rows_selected, colmap[1,]], selection=list(selected=matrix(c(1,2),1), mode='multiple', target='cell'),
                   options=list(pageLength=5, ordering=FALSE, initComplete=htmlwidgets::JS(
                       "function(settings, json) {",
                       "$(this.api().table().container()).css({'font-size': '80%'});",
                       "}")
                   )
-        ) %>% formatStyle(1, target="row", fontWeight="bold", color = colpal(input$plottab_rows_selected))
+        ) %>% formatStyle(1, target="row", fontWeight="bold") #, color = colpal(input$laplist_rows_selected))
     })
     
     output$speedplot <- renderPlot({
-        if (length(input$laplist_rows_selected) > 0 && length(input$plottab_cells_selected) > 0) {
-            lap <- laps[[input$laplist_rows_selected[input$plottab_cells_selected[1,1]]]]
-            plot(lap[,c(2,input$plottab_cells_selected[1,2])],type='l')
+        cs <<- input$plottab_cells_selected #matrix of the lap [,1] and value [,2] to plot in order
+        if (length(input$laplist_rows_selected) > 0 && length(cs) > 0) {
+            for (i in 1:length(cs[,1])) {
+                # cs[i,1] is the row in the table, index into the selected rows to find which lap
+                lap <- laps[[input$laplist_rows_selected[cs[i,1]]]]
+                if (i==1) {
+                    # create the plot, pick axes limits etc. column 2 is time in milliseconds
+                    plot(lap[,2]/1000.0, lap[,unpick(cs[i,2])],type='l',col=i,
+                         xlab="seconds", ylab=names(lap)[unpick(cs[i,2])],
+                         # ordered list of cells to plot, lookup which rows in laplist, and lap numbers from lapdf column 1
+                         main="")
+                } else {
+                    # add more lines to the plot
+                    lines(lap[,2]/1000.0, lap[,unpick(cs[i,2])],type='l',col=i)
+                }
+                # label the lap at the first data point, in the same color, with the right lap number
+                text(x=0, y=lap[1,unpick(cs[i,2])], labels=paste("Lap", lapdf[input$laplist_rows_selected[cs[i,1]],1],
+                    # if the line is different to the original y axis, label its data type
+                    ifelse(cs[1,2]==cs[i,2], "", names(lap)[unpick(cs[i,2])])), col=i, pos=4)
+            }
         }
     })
-    
-    output$tempplot <- renderPlot({
-        if (length(input$laplist_rows_selected) > 0 && length(input$plottab_cells_selected) > 0) {
-            lap <- laps[[input$laplist_rows_selected[input$plottab_cells_selected[1,1]]]]
-            plot(lap[,c(2,25)],type='l')
-        }
-    })}
+} #end of server
 
 # Run the application
 ptf(filename)
